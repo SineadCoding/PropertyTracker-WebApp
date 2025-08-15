2f017ff8a890dea2542e59d3139ec3ed4f4a929c
Sinead <sineaddejager@gmail.com>
2025-08-15 12:24:59 +0200

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -67,4 +67,5 @@ def refresh_listings():
     return jsonify({"success": True, "message": "Scraping started in background."})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
