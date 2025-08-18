a0696363aa3e0fdcd063769585d0ae3c7f5f6497
sinead <sineaddejager@gmail.com>
2025-08-18 16:19:11 +0200

    Revert to initial files from backup folder

--- models.py
+++ models.py
@@ -1,8 +1,2 @@
-class Property:
-    def __init__(self, title, price, location, agency, link, date):
-        self.title = title
-        self.price = price
-        self.location = location
-        self.agency = agency
-        self.link = link
-        self.date = date
+# Backup of models.py
+# ...original code will be copied here...
